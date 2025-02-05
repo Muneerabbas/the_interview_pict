9f184841c528ac4b0e1d9d534f9676dc5895e4ff
himanshug-08 <himanshugholse08@gmail.com>
2025-02-05 20:24:29 +0530

    Co-authored-by: nilaytayade <nilaytayade@users.noreply.github.com>

---
+++ app/_document.js
@@ -0,0 +1,31 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-EBQQJCL50P"
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-EBQQJCL50P');
+            `,
+          }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}

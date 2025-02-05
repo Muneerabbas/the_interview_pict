f3eb3b14fe08cbe02d04cae0ad0c9e387923c8fa
himanshug-08 <himanshugholse08@gmail.com>
2025-02-05 20:47:56 +0530

    Co-authored-by: nilaytayade <nilaytayade@users.noreply.github.com>

--- app/_document.js
+++ app/_document.js
@@ -1,26 +1,11 @@
-// pages/_document.js
+// app/_document.js
 import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        {/* Google tag (gtag.js) */}
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-EBQQJCL50P"
-        />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-
-              gtag('config', 'G-EBQQJCL50P');
-            `,
-          }}
-        />
+        <script>console.log('Custom _document.js is working!');</script>
       </Head>
       <body>
         <Main />

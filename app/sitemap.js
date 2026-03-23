345f034fcf6f21c74249b31ff9c95ec66633d324
Muneerabbas <muneer.abbas9595@gmail.com>
2026-03-23 23:56:26 +0530

    sitemap-added

---
+++ app/sitemap.js
@@ -0,0 +1,12 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://pict.live',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://pict.live/companies',
+      lastModified: new Date(),
+    },
+  ];
+}

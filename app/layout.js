89f31229b0333cd6ca0fa6555d5ebf0f052c379f
derpx06 <manaskhairnar1511@gmail.com>
2026-04-01 08:17:25 +0530

    some importan

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,21 @@
 // app/layout.js
+import { Inter } from "next/font/google";
 import "./globals.scss";
 import Providers from "./providers";
 
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-inter",
+});
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={inter.variable}>
       <head>
         <link rel="icon" type="image/png" href="/app_icon.png" />
         <meta name="google-adsense-account" content="ca-pub-9530051498159475" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9530051498159475" crossOrigin="anonymous"></script>
-
       </head>
       <body className="antialiased font-sans">
         <Providers>{children}</Providers>
